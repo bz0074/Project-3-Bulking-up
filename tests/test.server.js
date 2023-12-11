37640f8c435bc54fd9dd6f104d7eb75ff89bd4d2
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-12-11 11:32:25 -0600

    Update test.server.js

--- tests/test.server.js
+++ tests/test.server.js
@@ -8,7 +8,27 @@ describe('API Tests', () => {
     it('should return a 200 status for GET /', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message', 'Hello, welcome to the JWKS server!');
     });
 
-    // 
+    it('should handle POST /register with valid data', async () => {
+        const response = await request(app)
+            .post('/register')
+            .send({ username: 'testuser', email: 'test@example.com' });
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('password');
+    });
+
+    it('should handle POST /auth with valid data', async () => {
+        // Mock authentication logic and database setup as needed
+        const response = await request(app)
+            .post('/auth')
+            .send({ username: 'testuser', password: 'testpassword' });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Authentication successful');
+    });
+
+    //more test cases as needed
 });
