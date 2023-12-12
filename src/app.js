4b5fd1d2320a9da36789606b8d2051a35fa07ed2
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-12-11 22:06:27 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,38 +1,8 @@
 // app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
-app.use(bodyParser.json());
-
-// Connect to SQLite database
-const db = new sqlite3.Database(':memory:'); // Use ':memory:' for in-memory database
-
-// Create tables if they do not exist
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT NOT NULL UNIQUE,
-      password_hash TEXT NOT NULL,
-      email TEXT UNIQUE,
-      date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      last_login TIMESTAMP
-    )
-  `);
-
-  db.run(`
-    CREATE TABLE IF NOT EXISTS auth_logs (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      request_ip TEXT NOT NULL,
-      request_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      user_id INTEGER,
-      FOREIGN KEY(user_id) REFERENCES users(id)
-    )
-  `);
-});
-
 
 // Middleware
 app.use(bodyParser.json());
@@ -49,9 +19,7 @@ app.post('/register', (req, res) => {
 
 app.post('/auth', (req, res) => {
   // Handle authentication logic here
-  res.status(200).send('Authentication successful!');
+  res.status(200).send('Authentication successful');
 });
 
 module.exports = app;
-
-
