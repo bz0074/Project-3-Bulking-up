b189dcd2b33c94b744418c4db6b612f807649768
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-12-11 21:58:49 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,24 @@ db.serialize(() => {
   `);
 });
 
-// Your routes and other middleware go here
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, this is the root endpoint!');
+});
+
+app.post('/register', (req, res) => {
+  // Handle registration logic here
+  res.status(201).send('User registered successfully!');
+});
+
+app.post('/auth', (req, res) => {
+  // Handle authentication logic here
+  res.status(200).send('Authentication successful!');
+});
 
 module.exports = app;
 
