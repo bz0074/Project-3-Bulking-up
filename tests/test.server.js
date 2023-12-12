568391b30ea01b5593f1af8a4a0a6f42cc2d04f4
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-12-11 22:12:01 -0600

    Update test.server.js

--- tests/test.server.js
+++ tests/test.server.js
@@ -1,38 +1,47 @@
-// test.server.js
-
-// tests/test.server.js
-const expect = require('chai').expect;
 
 const request = require('supertest');
-const app = require('../src/app'); // Adjust the path based on the project structure
+const chai = require('chai');
+const expect = chai.expect;
+const app = require('../src/app');  // Adjust the path based on your project structure
 
-
-// Add  test cases here
 describe('API Tests', () => {
-    it('should return a 200 status for GET /', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).to.equal(200);
-        expect(response.body).toHaveProperty('message', 'Hello, welcome to the JWKS server!');
-    });
+  it('should return a 200 status for GET /', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).to.equal(200);
+  });
 
-    it('should handle POST /register with valid data', async () => {
-        const response = await request(app)
-            .post('/register')
-            .send({ username: 'testuser', email: 'test@example.com' });
+  it('should handle POST /register with valid data', async () => {
+    const response = await request(app)
+      .post('/register')
+      .send({
+        username: 'testuser',
+        email: 'testuser@example.com',
+      });
 
-        expect(response.statusCode).to.equal(201);
-        expect(response.body).toHaveProperty('password');
-    });
+    expect(response.status).to.equal(201);
+  });
 
-    it('should handle POST /auth with valid data', async () => {
-        // Mock authentication logic and database setup as needed
-        const response = await request(app)
-            .post('/auth')
-            .send({ username: 'testuser', password: 'testpassword' });
+  it('should handle POST /auth with valid data', async () => {
+    // Assume that you have registered a user with the username 'testuser' before testing authentication
+    // You may want to handle user registration in a separate test or manually in your application
+    const registrationResponse = await request(app)
+      .post('/register')
+      .send({
+        username: 'testuser',
+        email: 'testuser@example.com',
+      });
 
-        expect(response.statusCode).to.equal(200);
-        expect(response.text).to.equal('Authentication successful');
-    });
+    // Assuming registration was successful, proceed with authentication
+    const authenticationResponse = await request(app)
+      .post('/auth')
+      .send({
+        username: 'testuser',
+        //
+      });
 
-    //more test cases as needed
+    expect(authenticationResponse.status).to.equal(200);
+    expect(authenticationResponse.text).to.equal('Authentication successful');
+  });
 });
+
+
